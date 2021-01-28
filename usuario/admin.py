4d027b79a37214ccc7ddf9df8854ffc1c23fe037
kdelrio <kdelriom@gmail.com>
2021-01-28 09:59:45 -0300

    agregados models

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import Usuario,Empleado,Empresa_cliente
 
 admin.site.register(Usuario)
+admin.site.register(Empleado)
+admin.site.register(Empresa_cliente)
