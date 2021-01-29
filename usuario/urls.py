e98a844d830df92e16efb68efef1a3e50446635f
kdelrio <kdelriom@gmail.com>
2021-01-28 21:54:24 -0300

    primer crud completo

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import crear_usuario,inicio,lista,login,perfil,registro,eliminar_empleado,agregar
+from .views import crear_usuario,inicio,lista,login,perfil,registro,eliminar_empleado,agregar,modificar_empleado
 # Vistas propias de Django, para login y logout
 from django.contrib.auth.views import LoginView,LogoutView
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('crear_usuario/', views.crear_usuario, name='crear_usuario'),
     path('eliminar_empleado/<rut>/', eliminar_empleado, name='eliminar_empleados'),
+    path('modificar_empleado/<rut>/', modificar_empleado, name='modificar_empleado'),
     path('login/',LoginView.as_view(template_name='usuario/login.html'), name="InicioSesion"),
     path('logout/',LogoutView.as_view(template_name='usuario/inicio.html'),name="Logout"),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -23,29 +23,29 @@ def login(request):
     return render(request, 'usuario/login.html', {})
 
 def agregar(request):
+
+    if request.POST:
+        empleado = Empleado()
+        empleado.nombre = request.POST.get('nombre')
+        empleado.apellido = request.POST.get('apellido')
+        empleado.rut = request.POST.get('rut')
+        empleado.telefono = request.POST.get('telefono')
+        empleado.sbruto = request.POST.get('sbruto')
+        empleado.imposiciones = request.POST.get('imposiciones')
+        empleado.prevision = request.POST.get('prevision')
+        empleado.btransporte = request.POST.get('btransporte')
+        empleado.badc = request.POST.get('badc')
+
+        try:
+            empleado.save()
+        except:
+            mensaje = "No se ha podido eliminar"
+        return redirect('lista')
+
     return render(request, 'usuario/agregar.html', {})
 
 def registro(request):
     return render(request, 'usuario/registro.html', {})
-
-def agregar(request):
-
-    if request.POST:
-        empleado = Empleado()
-        nombre = request.POST.get('nombre'),
-        apellido = request.POST.get('apellido'),
-        rut = request.POST.get('rut'),
-        telefono = request.POST.get('telefono'),
-        sbruto = request.POST.get('sbruto'),
-        prevision = request.POST.get('prevision'),
-        imposiciones = request.POST.get('imposiciones'),
-        btransporte = request.POST.get('btransporte'),
-        badc = request.POST.get('badc')
-    try:
-        empleado.save()
-    except:
-            messages = 'no se pudo crear'
-    return redirect('lista') 
 
 def eliminar_empleado(request,rut):
 
@@ -56,3 +56,30 @@ def eliminar_empleado(request,rut):
     except:
         mensaje = "No se ha podido eliminar"
     return redirect('lista')
+
+def modificar_empleado(request,rut):
+
+    empleado = Empleado.objects.get(rut=rut)
+    variables = {
+        'empleado':empleado
+    }
+
+    if request.POST:
+        empleado = Empleado()
+        empleado.nombre = request.POST.get('nombre')
+        empleado.apellido = request.POST.get('apellido')
+        empleado.rut = request.POST.get('rut')
+        empleado.telefono = request.POST.get('telefono')
+        empleado.sbruto = request.POST.get('sbruto')
+        empleado.imposiciones = request.POST.get('imposiciones')
+        empleado.prevision = request.POST.get('prevision')
+        empleado.btransporte = request.POST.get('btransporte')
+        empleado.badc = request.POST.get('badc')
+
+        try:
+            empleado.save()
+        except:
+            mensaje = "No se ha podido eliminar"
+        return redirect('lista')
+
+    return render(request, 'usuario/modificar_empleado.html', variables)
